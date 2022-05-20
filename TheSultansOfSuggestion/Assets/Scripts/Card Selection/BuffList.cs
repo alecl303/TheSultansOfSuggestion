f0b13212120ca79079045525c74ad31fb4c73d69
cmpires <59940851+cmpires@users.noreply.github.com>
2022-05-19 19:03:40 -0700

    Update BuffList.cs

--- TheSultansOfSuggestion/Assets/Scripts/Card Selection/BuffList.cs
+++ TheSultansOfSuggestion/Assets/Scripts/Card Selection/BuffList.cs
@@ -6,29 +6,29 @@ using UnityEngine.SceneManagement;
 public class BuffList
 {
 
-    public List<Action> array = new List<Action>();
+    // public List<Action> array = new List<Action>();
 
-    void Start()
-    {    
-                
-    }
+    // void Start()
+    // {    
 
-    void BuffList()
-    {
-        array = array[functions]
-    }
+    // }
 
-    void Buff1()
-    {
+    // void BuffList()
+    // {
+    //     array = array[functions];
+    // }
 
-    }
-    void Buff2()
-    {
+    // void Buff1()
+    // {
 
-    }
-    void Buff3()
-    {
+    // }
+    // void Buff2()
+    // {
 
-    }
+    // }
+    // void Buff3()
+    // {
+
+    // }
 
 }
