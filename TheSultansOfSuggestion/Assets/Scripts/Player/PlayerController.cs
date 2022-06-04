b4a8908d42218af4a6b0899d5ebf1ea30e566fe8
alecl303 <alecliang303@gmail.com>
2022-06-03 22:48:20 -0700

    chagned default spell

--- TheSultansOfSuggestion/Assets/Scripts/Player/PlayerController.cs
+++ TheSultansOfSuggestion/Assets/Scripts/Player/PlayerController.cs
@@ -51,7 +51,7 @@ public class PlayerController : MonoBehaviour
         this.left = ScriptableObject.CreateInstance<MoveCharacterLeft>();
         this.up = ScriptableObject.CreateInstance<MoveCharacterUp>();
         this.down = ScriptableObject.CreateInstance<MoveCharacterDown>();
-        this.activeSpell1 = ScriptableObject.CreateInstance<Invincibility>();
+        this.activeSpell1 = ScriptableObject.CreateInstance<SpellNothing>();
         
         this.roll = ScriptableObject.CreateInstance<Roll>();
 
